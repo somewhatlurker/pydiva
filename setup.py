f906b9d1180909bf10956f7d02c670a679b51c35
somewhatlurker <52014015+somewhatlurker@users.noreply.github.com>
2020-12-24 08:39:35 +1100

    change from whitelisted pydiva to blacklisted tests in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/somewhatlurker/pydiva",
-    packages=['pydiva'],
+    packages=setuptools.find_packages(exclude=['tests*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
