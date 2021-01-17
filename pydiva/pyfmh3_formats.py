4b59cf04412ea93d8266b40c8beace43a9dbb0c4
somewhatlurker <52014015+somewhatlurker@users.noreply.github.com>
2021-01-18 03:14:03 +1100

    pyfmh3: reduce overall complexity by consolidating file formats and handing fonm stuff off to new cs3_file_utils

--- pydiva/pyfmh3_formats.py
+++ pydiva/pyfmh3_formats.py
@@ -2,8 +2,6 @@
 FMH3 format information for pyfmh3
 """
 
-from construct import Struct, Tell, Const, Padding, Int32ul, RepeatUntil, CString, Pointer, Byte, Int16ul, Flag, Int64ul, Bytes, Seek, If
-
 _construct_version = None
 try:
     from construct import __version__ as _construct_version
@@ -18,130 +16,89 @@ if _construct_version:
     if (_construct_version[0] < 2) or ((_construct_version[0] == 2) and (_construct_version[1] < 9)):
         raise Exception('Construct version too low, please install version 2.9+')
 
+from construct import Struct, Tell, Const, Padding, Padded, Pointer, RepeatUntil, Byte, Int16ul, Flag, Rebuild, If, Int32ub, Seek, Int32ul, Int64ul
+from pydiva.util.cs3_file_utils import RelocationPointerAdapter, gen_relocation_struct, relocation_data_len, gen_eofc_struct, gen_cs3_file
 
-_fmh3_format = Struct(
-    "pointer_offset" / Tell,
-    "signature" / Const(b'FMH3'),
-    Padding(4),
-    "fonts_count" / Int32ul,
-    "fonts_pointers_offset" / Int32ul,
-    "fonts" / Pointer(lambda this: this.fonts_pointers_offset + this.pointer_offset, RepeatUntil(lambda obj,lst,ctx: ctx._index >= ctx.fonts_count - 1, Struct(
-        "pointer" / Int32ul,
-        "data" / Pointer(lambda this: this.pointer + this._.pointer_offset, Struct(
-            "id" / Int32ul,
-            "advance_width" / Byte,
-            "line_height" / Byte,
-            "box_width" / Byte,
-            "box_height" / Byte,
-            "layout_param_1" / Byte, 
-            "layout_param_2_numerator" / Byte,
-            "layout_param_2_denominator" / Byte,
-            Padding(1),
-            "other_params?" / Int32ul,
-            "tex_size_chars" / Int32ul,
-            "chars_count" / Int32ul,
-            "chars_pointer" / Int32ul,
-            "chars" / Pointer(lambda this: this.chars_pointer + this._._.pointer_offset, RepeatUntil(lambda obj,lst,ctx: ctx._index >= ctx.chars_count - 1, Struct(
-                "codepoint" / Int16ul,
-                "halfwidth" / Flag,
+def _gen_fmh3_struct(int_type, pointer_type):
+    return Struct(
+        "pointer_offset" / Tell,
+        "signature" / Const(b'FMH3'),
+        Padding(4),
+        "fonts_count" / Padded(pointer_type.sizeof(), int_type),
+        "fonts_pointers_offset" / pointer_type,
+        "fonts" / Pointer(lambda this: this.fonts_pointers_offset + this.pointer_offset, RepeatUntil(lambda obj,lst,ctx: ctx._index >= ctx.fonts_count - 1, Struct(
+            "pointer" / pointer_type,
+            "data" / Pointer(lambda this: this.pointer + this._.pointer_offset, Struct(
+                "id" / int_type,
+                "advance_width" / Byte,
+                "line_height" / Byte,
+                "box_width" / Byte,
+                "box_height" / Byte,
+                "layout_param_1" / Byte, 
+                "layout_param_2_numerator" / Byte,
+                "layout_param_2_denominator" / Byte,
                 Padding(1),
-                "tex_col" / Byte,
-                "tex_row" / Byte,
-                "glyph_x" / Byte,
-                "glyph_width" / Byte,
-            ))),
+                "other_params?" / int_type,
+                "tex_size_chars" / int_type,
+                "chars_count" / int_type,
+                "chars_pointer" / pointer_type,
+                "chars" / Pointer(lambda this: this.chars_pointer + this._._.pointer_offset, RepeatUntil(lambda obj,lst,ctx: ctx._index >= ctx.chars_count - 1, Struct(
+                    "codepoint" / Int16ul,
+                    "halfwidth" / Flag,
+                    Padding(1),
+                    "tex_col" / Byte,
+                    "tex_row" / Byte,
+                    "glyph_x" / Byte,
+                    "glyph_width" / Byte,
+                ))),
+            )),
+        ))),
+    )
+
+def _gen_fonm_struct(int_type, pointer_type):
+    return Struct(
+        "pointer_offset" / Tell,
+        "signature" / Const(b'FONM'),
+        "section_size_offset" / Tell,
+        "section_size" / Rebuild(int_type, lambda this: 0), # this is written later when building
+        "data_pointer" / int_type,
+        "flags" / Const(0x18000000 if int_type == Int32ub else 0x10000000, int_type),
+        "depth" / Const(0, int_type),
+        "data_size" / int_type,
+        "data" / Pointer(lambda this: this.data_pointer + this.pointer_offset, _gen_fmh3_struct(int_type, RelocationPointerAdapter(pointer_type))),
+        # Seek(lambda this: this.data_pointer + this.data_size),
+        # ENRS (endian reversal) ignored
+        "extra_sections" / If(lambda this: this._building, Struct(
+            "POF" / Pointer(lambda this: this._.data_pointer + this._.data_size + this._.pointer_offset, gen_relocation_struct(int_type, pointer_type)),
+            # dirty hack to rewrite section_size (POF data size is unavailable during initial build)
+            "build_pos" / Tell,
+            Seek(lambda this: this._.section_size_offset),
+            "section_size_proper" / Rebuild(int_type, lambda this: this._.data_size + relocation_data_len(this.POF.data) + 32 + 32),
+            Seek(lambda this: this.build_pos),
+            
+            Pointer(lambda this: this._.data_pointer + this._.data_size + this._.pointer_offset + relocation_data_len(this.POF.data) + 32, gen_eofc_struct(int_type)),
+            Pointer(lambda this: this._.data_pointer + this.section_size_proper + this._.pointer_offset, gen_eofc_struct(int_type)),
         )),
-    ))),
-)
-
-_fmh3_int64_format = Struct(
-    "pointer_offset" / Tell,
-    "signature" / Const(b'FMH3'),
-    Padding(4),
-    "fonts_count" / Int64ul,
-    "fonts_pointers_offset" / Int64ul,
-    "fonts" / Pointer(lambda this: this.fonts_pointers_offset + this.pointer_offset, RepeatUntil(lambda obj,lst,ctx: ctx._index >= ctx.fonts_count - 1, Struct(
-        "pointer" / Int64ul,
-        "data" / Pointer(lambda this: this.pointer + this._.pointer_offset, Struct(
-            "id" / Int32ul,
-            "advance_width" / Byte,
-            "line_height" / Byte,
-            "box_width" / Byte,
-            "box_height" / Byte,
-            "layout_param_1" / Byte, 
-            "layout_param_2_numerator" / Byte,
-            "layout_param_2_denominator" / Byte,
-            Padding(1),
-            "other_params?" / Int32ul,
-            "tex_size_chars" / Int32ul,
-            "chars_count" / Int32ul,
-            "chars_pointer" / Int32ul,
-            "chars" / Pointer(lambda this: this.chars_pointer + this._._.pointer_offset, RepeatUntil(lambda obj,lst,ctx: ctx._index >= ctx.chars_count - 1, Struct(
-                "codepoint" / Int16ul,
-                "halfwidth" / Flag,
-                Padding(1),
-                "tex_col" / Byte,
-                "tex_row" / Byte,
-                "glyph_x" / Byte,
-                "glyph_width" / Byte,
-            ))),
-        )),
-    ))),
-)
-
-_pof1_struct = Struct(
-    "pointer_offset" / Tell,
-    "signature" / Const(b'POF1'),
-    "section_size" / Int32ul,
-    "data_pointer" / Const(32, Int32ul),
-    "flags" / Const(b'\x00\x00\x00\x10'),
-    "depth" / Const(0, Int32ul),
-    "data_size" / Int32ul,
-    Padding(8),
-    # hardcoded offset for this instead of pointer
-    "data" / Bytes(lambda this: this.data_size)
-)
-
-_eofc_struct = Struct(
-    "pointer_offset" / Tell,
-    "signature" / Const(b'EOFC'),
-    "section_size" / Const(0, Int32ul),
-    "data_pointer" / Const(32, Int32ul),
-    "flags" / Const(b'\x00\x00\x00\x10'),
-    "depth" / Const(0, Int32ul),
-    "data_size" / Const(0, Int32ul),
-    Padding(8)
-)
-
-_fonm_format = Struct(
-    "pointer_offset" / Tell,
-    "signature" / Const(b'FONM'),
-    "section_size" / Int32ul,
-    "data_pointer" / Int32ul,
-    "flags" / Const(b'\x00\x00\x00\x10'),
-    "depth" / Const(0, Int32ul),
-    "data_size" / Int32ul,
-    "data" / Pointer(lambda this: this.data_pointer + this.pointer_offset, _fmh3_int64_format),
-    # Seek(lambda this: this.data_pointer + this.data_size),
-    # ENRS (endian reversal) ignored
-    "extra_sections" / If(lambda this: this._building, Struct(
-        "pof1" / Pointer(lambda this: this._.data_pointer + this._.data_size + this._.pointer_offset, _pof1_struct),
-        Pointer(lambda this: this._.data_pointer + this._.data_size + this._.pointer_offset + this.pof1_size, _eofc_struct),
-        Pointer(lambda this: this._.data_pointer + this._.section_size + this._.pointer_offset, _eofc_struct)
-    )),
-)
+    )
 
 _fmh3_types = {
     'FMH3': {
         'remarks': 'unencapsulated FT fontmap',
-        'struct': _fmh3_format,
+        'struct': _gen_fmh3_struct(Int32ul, Int32ul),
         'address_size': 4,
         'nest_fmh3_data': False,
     },
     'FONM': {
         'remarks': 'X fontmap in FONM container',
-        'struct': _fonm_format,
+        #'struct': _gen_fonm_struct(Int32ul, Int64ul),
+        'struct': gen_cs3_file(Int32ul, Int64ul, [{
+            'signature': 'FONM',
+            'data_size': lambda this: this.data_size,
+            'data_subcon': _gen_fmh3_struct(Int32ul, RelocationPointerAdapter(Int64ul)),
+            'enrs': True,
+            'relocation': True
+        }]),
         'address_size': 8,
-        'nest_fmh3_data': True,
+        'nest_fmh3_data': 'FONM',
     },
 }
---
+++ pydiva/util/cs3_file_utils.py
@@ -0,0 +1,289 @@
+"""
+Stuff for dealing with sectioned from CS3 engine games. (F2nd, X)
+
+Only single-section binary files (eg. FONM, DSC) + relocation are tested, so
+other stuff is probably wrong, but the intent is that is can be expanded in the
+future.
+"""
+
+from construct import Adapter, Struct, Tell, Const, Rebuild, Int32ub, Padding, Bytes, Default, IfThenElse, If, Seek, Probe
+
+def gen_relocation_data(offsets):
+    """
+    Given a list of offsets (offsets are in multiples of pointer length),
+    output bytes to put in the relocation section's data.
+    """
+    
+    last_offset = 0
+    out = b''
+    for o in offsets:
+        distance = o - last_offset
+        
+        if distance > 0x3ff:
+            b = distance.to_bytes(4, byteorder='big')
+            b = bytes([b[0] | 0xc0]) + b[1:]
+        elif distance > 0x3f:
+            b = distance.to_bytes(2, byteorder='big')
+            b = bytes([b[0] | 0x80]) + b[1:]
+        else:
+            b = distance.to_bytes(1, byteorder='big')
+            b = bytes([b[0] | 0x40]) + b[1:]
+        
+        out += b
+        last_offset = o
+    
+    out_size = len(out) + 4
+    out = out_size.to_bytes(4, byteorder='little', signed=False) + out # prepend length
+    
+    # pad to 16 bytes
+    while len(out) % 16:
+        out += b'\x00'
+    
+    return out
+
+def relocation_data_len(offsets):
+    """
+    Get the length of relocation data.
+    (implemnted by calling len on gen_relocation_data result)
+    """
+    
+    return len(gen_relocation_data(offsets))
+
+class RelocationPointerAdapter(Adapter):
+    """
+    Wraps the pointer type, and generates relocation data during building.
+    
+    Relocation data will be put in root context's extra_sections['pof1']['data']
+    (root context is up one level from level that contains pointer_offset)
+    """
+    
+    def _decode(self, obj, context, path):
+        # called at parsing time to return a modified version of obj
+        
+        return obj
+
+    def _encode(self, obj, context, path):
+        # called at building time to return a modified version of obj
+        
+        io_offset = context._io.tell()
+        
+        # try to find the current context's pointer offset by recursing up
+        pointer_offset_ctx = context
+        while hasattr(pointer_offset_ctx, '_') and not hasattr(pointer_offset_ctx, 'pointer_offset'):
+            pointer_offset_ctx = pointer_offset_ctx._
+        pointer_offset = getattr(pointer_offset_ctx, 'pointer_offset', 0)
+        
+        offset = io_offset - pointer_offset
+        pointer_size = self.subcon.sizeof()
+        if offset % pointer_size != 0:
+            raise Exception('Pointer at bad offset -- can\'t generate relocation data')
+        offset = offset // pointer_size
+        
+        # set offset to pointer_relocation_offsets in the parent context of pointer_offset
+        # -- directly in the POF section's data (DIRTY SOLUTION!)
+        # (doesn't make sense to not have this when using relocation tho)
+        if not 'extra_sections' in pointer_offset_ctx._:
+            pointer_offset_ctx._['extra_sections'] = {}
+        if not 'POF' in pointer_offset_ctx._['extra_sections']:
+            pointer_offset_ctx._['extra_sections']['POF'] = {}
+        if not 'data' in pointer_offset_ctx._['extra_sections']['POF']:
+            pointer_offset_ctx._['extra_sections']['POF']['data'] = []
+        
+        pointer_offset_ctx._['extra_sections']['POF']['data'] += [offset]
+        
+        pointer_offset_ctx._['extra_sections']['POF']['data'].sort()
+        
+        return obj
+
+class RelocationDataAdapter(Adapter):
+    """Converts a list of pointer offsets into relocation section data."""
+    
+    def _decode(self, obj, context, path):
+        # called at parsing time to return a modified version of obj
+        
+        return obj
+
+    def _encode(self, obj, context, path):
+        # called at building time to return a modified version of obj
+        
+        return gen_relocation_data(obj)
+
+def gen_relocation_struct(int_type, pointer_type, depth):
+    """
+    Generates a Construct struct for a POFx (relocation) section.
+    
+    Required context layout for data is:
+    ```
+    data=relocation_offsets
+    ```
+    """
+    
+    return Struct(
+        "pointer_offset" / Tell,
+        "signature" / Const(b'POF0' if pointer_type.sizeof() == 4 else b'POF1'),
+        "section_size" / Rebuild(int_type, lambda this: relocation_data_len(this.data)),
+        "data_pointer" / Default(int_type, 32),
+        "flags" / Const(0x18000000 if int_type == Int32ub else 0x10000000, int_type),
+        "depth" / Rebuild(int_type, depth),
+        "data_size" / Rebuild(int_type, lambda this: this.section_size),
+        Padding(lambda this: this.data_pointer - 24),
+        Seek(lambda this: this.data_pointer + this.pointer_offset),
+        "data" / RelocationDataAdapter(Bytes(lambda this: this.data_size))
+    )
+
+def gen_eofc_struct(int_type, depth, fix_parent_size=None):
+    """Generates a Construct struct for an EOFC (end of file) section."""
+    
+    return Struct(
+        "pointer_offset" / Tell,
+        "signature" / Const(b'EOFC'),
+        "section_size" / Rebuild(int_type, 0),
+        "data_pointer" / Rebuild(int_type, 32),
+        "flags" / Const(0x18000000 if int_type == Int32ub else 0x10000000, int_type),
+        "depth" / Rebuild(int_type, depth),
+        "data_size" / Rebuild(int_type, 0),
+        Padding(lambda this: this.data_pointer - 24),
+        Seek(lambda this: this.data_pointer + this.pointer_offset),
+        Rebuild(Bytes(lambda this: this.data_size), b''),
+        "parent_size_fix" / If(fix_parent_size, Struct(
+            "end_offset" / Tell,
+            Seek(lambda this: this._._.section_outer.section.section_size_offset),
+            Rebuild(int_type, lambda this: this.end_offset - (this._._.section_outer.section.data_pointer + this._._.section_outer.section.pointer_offset)),
+            Seek(lambda this: this.end_offset)
+        ))
+    )
+
+def gen_section_struct(int_type, pointer_type, signature, depth, data_size=0, data_subcon=Bytes(lambda this: this.data_size), enrs=False, relocation=False, optional=False):
+    """
+    Generates a Construct scruct for an arbitrary section, with user-defined
+    signature, data_size (can be lambda), and data subcon.
+    enrs and relocation are optional (enrs only so parsing won't fail), and the
+    entire section can be made optional.
+    
+    Optional sections are skipped if not present during parsing, and always
+    skipped during building.
+    
+    To read data_size from section dict when building, you can use
+    `lambda this: this.data_size`.
+    
+    Required context layout for data is:
+    ```
+    section_outer=dict(
+        section=dict(
+            data_pointer=32,
+            data=b'Sample Data',
+            extra_sections=dict() # needed to avoid KeyError during build with relocation
+        )
+    )
+    ```
+    """
+    
+    return "section_outer" / If(lambda this: this._parsing or not optional, Struct(
+        "signature_first_pass" / Rebuild(Bytes(4), signature.encode('ascii')),
+        Seek(lambda this: this._io.tell() - 4),
+        "section" / If(lambda this: this.signature_first_pass == signature.encode('ascii') or not optional, Struct(
+            "pointer_offset" / Tell,
+            "signature" / Const(signature.encode('ascii')),
+            "section_size_offset" / Tell, # for fixing section_size later 
+            "section_size" / Default(int_type, 0),
+            "data_pointer" / Default(int_type, 32),
+            #Probe(),
+            "flags" / Const(0x18000000 if int_type == Int32ub else 0x10000000, int_type),
+            "depth" / Rebuild(int_type, depth),
+            "data_size" / Rebuild(int_type, data_size),
+            Padding(lambda this: this.data_pointer - 24),
+            Seek(lambda this: this.data_pointer + this.pointer_offset),
+            "data" / Default(data_subcon, b''),
+            Seek(lambda this: this.data_pointer + this.data_size + this.pointer_offset),
+            "extra_sections" / Struct(
+                # I think enrs may come before or after relocation(?), so just try reading it from both
+                gen_section_struct(int_type, pointer_type, 'ENRS', depth, optional=True) if enrs else Tell,
+                "POF" / gen_relocation_struct(int_type, pointer_type, depth) if relocation else Tell,
+                gen_section_struct(int_type, pointer_type, 'ENRS', depth, optional=True) if enrs else Tell,
+                "end_offset" / Tell,
+                Seek(lambda this: this._.section_size_offset),
+                Rebuild(int_type, lambda this: this.end_offset - (this._.data_pointer + this._.pointer_offset)),
+                Seek(lambda this: this.end_offset)
+            ),
+        ))
+    ))
+
+def gen_cs3_sections(int_type, pointer_type, sections, depth=0):
+    """
+    Generates a Construct scruct for multiple sections, including children,
+    from a list of info dicts.
+    
+    My understanding is that these should be structured in the file like:
+    [
+        SECTION_MAIN
+        SECTION_ENRS
+        SECTION_POF
+        SECTION_CHILDREN // may have nested complete sections
+        SECTION_EOF
+    ]
+    but this is based only on skimming some MikuMikuLibrary code and I never checked it lol
+    
+    Info dicts must have:
+    signature
+    
+    Info dicts may have:
+    data_size, data_subcon (by default, will use 0 length and Bytes),
+    enrs, relocation (set to enable sections - enrs only so parsing won't fail),
+    optional (set to make this section optional to read, and disable building),
+    child_sections (info list for this section's children)
+    
+    To read data_size from section dict when building, you can use
+    `lambda this: this.data_size`.
+    
+    Required context layout for data is:
+    (well, intended context...  this isn't very tested yet)
+    ```
+    '[SIGNATURE]': {
+        section_outer=dict(
+            section=dict(
+                data_pointer=32,
+                data=b'Sample Data',
+                extra_sections=dict() # needed to avoid KeyError during build with relocation
+            )
+        ),
+        '[CHILD0_SIG]'=dict(
+            section_outer=dict(
+                section=dict(
+                    data_pointer=32,
+                    data=b'Sample Data',
+                    extra_sections=dict() # needed to avoid KeyError during build
+                )
+            ),
+            '[CH0_CH1_SIG]'=dict(
+                ...
+            )
+        )
+        ...
+    }
+    ```
+    """
+    
+    if len(sections) == 0:
+        return Struct()
+    
+    struct = Struct()
+    for s in sections:
+        ss = gen_section_struct(int_type, pointer_type, s['signature'], depth, s.get('data_size', 0), s.get('data_subcon', Bytes(lambda this: this.data_size)), s.get('enrs'), s.get('relocation'), s.get('optional'))
+        
+        cs = gen_cs3_sections(int_type, pointer_type, s.get('child_sections', []), depth + 1)
+        ss += cs
+        
+        ss += "EOFC" / gen_eofc_struct(int_type, depth, s['signature'])
+        struct += s['signature'] / ss
+    
+    return struct
+
+def gen_cs3_file(int_type, pointer_type, sections):
+    """
+    Generates a Construct scruct for a file with multiple sections, including
+    children, from a list of info dicts.
+    
+    Same as gen_cs3_sections but with an end of file section appended.
+    """
+    
+    return gen_cs3_sections(int_type, pointer_type, sections) + gen_eofc_struct(int_type, 0)
